dd0c520a14a554986964846df745f9902623ec54
liang.he@intel.com <liang.he@intel.com>
2022-01-27 02:59:06 +0800

    update LICENSE and few project files

--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name="wamr-python",
+    version="0.1.0",
+    description="A WebAssembly runtime powered by WAMR",
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author="The WAMR Project Developers",
+    author_email="hello@bytecodealliance.org",
+    url="https://github.com/lum1n0us/wamr-python",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=["wamr"],
 )
-
